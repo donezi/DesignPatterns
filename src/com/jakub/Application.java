682010799b5c9c5365255f48cf7cac0dacb88793
donezi <jakub.piechocki@gmail.com>
2017-06-06 18:28:38 +0200

    Some other lambda example

--- src/com/jakub/Application.java
+++ src/com/jakub/Application.java
@@ -1,7 +1,38 @@
 package com.jakub;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+
+
+        List<List<String>> nameList = Arrays.asList(Arrays.asList("Jakub", "Marcin", "Andrzej"),
+                Arrays.asList("Anna", "Antonina", "Małgorzata"));
+
+        nameList.stream()
+                .flatMap(e->e.stream())
+                .filter(e-> e.startsWith("A"))
+                .forEach(e-> System.out.println(e));
+
+        Map<String, List<String>>map = new HashMap<>();
+        map.put("men", Arrays.asList("Jakub", "Marcin", "Andrzej"));
+        map.put("women", Arrays.asList("Anna", "Antonina", "Małgorzata"));
+
+        map.entrySet()
+                .stream()
+                .flatMap(e-> e.getValue().stream())
+//                .map(e-> e.getValue())
+//                .flatMap(e-> e.stream())
+                .filter(UserPredicateFactory.nameStartsWith("A"))
+                .forEach(e-> System.out.println(e));
+
+
+
+        List<String> names = Arrays.asList("Szymon", "Jakub", "Anna", "Kasia");
+//        System.out.println("Hello World");
     }
 }
---
+++ src/com/jakub/UserPredicateFactory.java
@@ -0,0 +1,18 @@
+package com.jakub;
+
+
+import java.util.function.Predicate;
+
+public class UserPredicateFactory {
+    // lambda - anonimowa implmentacja interfajsu z jedną metodą
+    public static Predicate<String> nameStartsWith (String prefix) {
+//        return new Predicate<String>() {
+//            @Override
+//            public boolean test(String s) {
+//                return s.startsWith(prefix);
+//            }
+//        };
+//        OR
+        return s -> s.startsWith(prefix);
+    }
+}
